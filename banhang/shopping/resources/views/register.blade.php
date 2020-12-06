790a78176c1038f1305767014865fd4385617fa9
phanphucan48 <phanphucan4890@gmail.com>
2020-12-06 19:12:20 +0700

    update

---
+++ banhang/shopping/resources/views/register.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+<form>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Ten</label>
+        <input type="text" class="form-control" name="name">
+
+    </div>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email </label>
+        <input type="email" class="form-control" name="email" aria-describedby="emailHelp">
+
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Mat khau</label>
+        <input type="password" class="form-control" name="password">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">nhap lai Mat khau</label>
+        <input type="password" class="form-control" name="password">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">anh</label>
+        <input type="file" class="form-control" name="image">
+    </div>
+
+    <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" name="role">
+
+        <label class="form-check-label" for="exampleCheck1" >admin</label>
+    </div>
+    <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" name="role">
+
+        <label class="form-check-label" for="exampleCheck1" >user</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+
+</form>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</body>
+</html>
